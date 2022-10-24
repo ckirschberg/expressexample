4683a1e771b9d3bc55b33f32f5ce5c7418c4c1c1
Christian Kirschberg <ckirschberg@gmail.com>
2022-10-24 10:41:24 +0200

    added user schema

---
+++ schemas/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('User', UserSchema);
